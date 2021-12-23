bc0606cf87f25818c7876bb5315c027a7e63c0cc
Andreas Schmid <andreas.schmid@ur.de>
2021-12-23 22:45:36 +0100

    run.sh now supports multiple arguments for batch processing

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-path=$1
+for path in "$@"
+do
+    # extract annotations based on RGB scan and IR scan
+    python3 extraction.py "$path"
 
-# extract annotations based on RGB scan and IR scan
-python3 extraction.py "$path"
-
-# insert extracted annotations into PDF file
-python3 insert_annotation.py "$path" "bias.png"
+    # insert extracted annotations into PDF file
+    python3 insert_annotation.py "$path" "bias.png"
+done
